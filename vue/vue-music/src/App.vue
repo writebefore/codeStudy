98ed8a7cb41d8641f5a9afc1a1bb533517dc516f
乱了的和弦 <2324262941@qq.com>
2020-09-07 21:30:32 +0800

    vue-music

---
+++ vue/vue-music/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header.vue'
+
+export default {
+  components: {
+    'v-header': Header,
+  }
+};
+
+</script>
+
+<style lang="stylus">
+
+</style>
